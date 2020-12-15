c8d330769bd8fca5286c077beffc037d3be00c1c
paulclement-cmd <paulclement.lr@gmail.com>
2020-12-15 01:42:08 +0000

    ‘commit’

---
+++ soso.js
@@ -0,0 +1,16 @@
+let planets = ['Terre','Jupiter','Saturne','Mars','Uranus'];
+
+for (let i = 0; i < planets.length; i++) {
+  console.log(planets[i])
+}
+
+
+
+let hello = function (world) {
+    world = 'Hello World !'
+    return world
+
+}
+
+
+console.log(hello())
